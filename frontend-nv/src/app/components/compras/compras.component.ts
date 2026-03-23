c964272c897b9baa574cc10ab71f3a7bc623e518
Jose Arod Martinez Hernandez <160168999+Joseamh12345678@users.noreply.github.com>
2026-03-23 12:38:56 -0600

    deploy2

--- frontend-nv/src/app/components/compras/compras.component.ts
+++ frontend-nv/src/app/components/compras/compras.component.ts
@@ -19,7 +19,7 @@ export class ComprasComponent {
   uidFirebase: string = 'demoUID';
 
   // 🔥 URL API (LOCAL por ahora)
-  apiUrl: string = 'https://asterplay.railway.app/compras';
+  apiUrl: string = 'rentavideojuegospeliculas-production.up.railway.app/compras';
 
   constructor(
     private fb: FormBuilder,
